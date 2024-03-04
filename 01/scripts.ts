2fca2f9d2210e999be99f9800fa15f36b448a3e8
Ana Beatriz Barreto <abbmr@hotmail.com>
2024-03-04 20:22:08 -0300

    done

--- 01/scripts.ts
+++ 01/scripts.ts
@@ -3,8 +3,11 @@ type Usuario = {
     email: string,
     cpf: string,
     rg: string
-    dataNacimento?: string
+    dataNascimento?: string
 }
+
+type Usuario2 = Omit<Usuario, 'rg'>
+type Usuario2Obrigatorio = Required<Usuario2>
 
 const cadastrarUsuário = (info: Usuario): Usuario => {
     return info;
--- 02/scripts.ts
+++ 02/scripts.ts
@@ -19,3 +19,25 @@ type Carrinho = {
     tipoTransacao: 'credito' | 'Debito',
     cartao: Cartao
 }
+
+type Endereco = {
+    cep: string,
+    rua: string,
+    bairro: string,
+    cidade: string,
+    estado: string
+}
+
+type NewCarrinho = {
+    item: Item,
+    qtd: number,
+    desconto: number,
+    frete: number,
+    tipoTransacao: 'credito' | 'debito',
+    cartao: Cartao,
+    endereco: Endereco
+}
+
+type NewCarrinho2 = Carrinho & {
+    endereco: Endereco
+}
